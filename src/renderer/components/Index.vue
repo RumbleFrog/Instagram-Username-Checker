5e9bd4465cc4a6d436a437ce65421769ce4f3573
RumbleFrog <xxlosttechxx@gmail.com>
2018-02-04 18:15:46 -0500

    Isolate Phases

--- src/renderer/components/Index.vue
+++ src/renderer/components/Index.vue
@@ -1,26 +1,30 @@
 <template>
     <div class="container has-text-centered">
-        <b-field>
-            <b-upload v-model="dropFiles" multiple drag-drop accept="text/plain">
-                <section class="section">
-                    <div class="content has-text-centered">
-                        <p>
-                            <b-icon
-                                    icon="upload"
-                                    size="is-large">
-                            </b-icon>
-                        </p>
-                        <p>Drop your username list here or click to upload</p>
-                    </div>
-                </section>
-            </b-upload>
-        </b-field>
+        <div id="parsing" v-if="checking == false">
+            <b-field>
+                <b-upload v-model="dropFiles" multiple drag-drop accept="text/plain">
+                    <section class="section">
+                        <div class="content has-text-centered">
+                            <p>
+                                <b-icon
+                                        icon="upload"
+                                        size="is-large">
+                                </b-icon>
+                            </p>
+                            <p>Drop your username lists here or click to upload</p>
+                        </div>
+                    </section>
+                </b-upload>
+            </b-field>
 
-        <div class="field" v-for="(file, index) in dropFiles" :key="index">
-            <b-tag type="is-primary">
-                {{ file.name }}
-                <button class="delete is-small" type="button" @click="deleteDropFile(index)"></button>
-            </b-tag>
+            <div class="field" v-for="(file, index) in dropFiles" :key="index">
+                <b-tag type="is-primary">
+                    {{ file.name }}
+                    <button class="delete is-small" type="button" @click="deleteDropFile(index)"></button>
+                </b-tag>
+            </div>
+
+            <button class="button is-primary" :class="{'is-loading': parsing}" @click="parse()">Parse Files</button>
         </div>
     </div>
 </template>
@@ -40,6 +44,9 @@
       methods: {
         deleteDropFile(index) {
           this.dropFiles.splice(index, 1);
+        },
+        parse() {
+          this.parsing = true;
         },
       },
       name: 'index',
